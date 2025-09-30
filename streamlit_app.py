9904d0dd1a423ff6b3ee62f020c2ae2f99fcea1b
Sophy-Soun <108968775+Sophy-Soun@users.noreply.github.com>
2025-09-30 11:33:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,19 +18,19 @@ with st.expander("Data"):
   st.write('**y**')
   y_raw = df.genre
   y_raw
-with st.expander('Data Visulization'):
-  # "bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
-  st.scatter_chart(data = df, x ='age', y = 'gender')
+# with st.expander('Data Visulization'):
+#   # "bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+#   st.scatter_chart(data = df, x ='age', y = 'gender')
 
-# # Input Features
-# with st.sidebar:
-#   st.header('Input Features')
-#   island = st.selectbox('Island', ('Biscoe','Drean','Tergernson'))
-#   bill_length_mm = st.slider('Bill Length (mm)',32.1,59.6, 43.9)
-#   bill_dept_mm = st.slider('Bill Depth (mm)',13.1, 21.5, 17.2)
-#   flipper_mm = st.slider('Fipper Length (mm)', 172.0, 231.0, 201.0)
-#   body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
-#   gender = st.selectbox('Gender', ('male', 'female'))
+# Input Features
+with st.sidebar:
+  st.header('Input Features')
+  island = st.selectbox('Island', ('Biscoe','Drean','Tergernson'))
+  bill_length_mm = st.slider('Bill Length (mm)',32.1,59.6, 43.9)
+  bill_dept_mm = st.slider('Bill Depth (mm)',13.1, 21.5, 17.2)
+  flipper_mm = st.slider('Fipper Length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
 
 # # Create the DataFrame for input features
 # data = {'island': island,
